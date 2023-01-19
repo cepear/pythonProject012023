dddafaff141e515811835d8f92b205e0b8c10ff3
iroda9900 <ishukurova0311@gmail.com>
2023-01-18 21:17:17 -0500

    forms scenario manual test case steps added

--- webelement_class/forms.py
+++ webelement_class/forms.py
@@ -17,7 +17,21 @@ try:
     print("-----Starting test with WebDriver properties------------")
     driver.get(host)
     time.sleep(2)
-
+    # enter first_name = 'john', enter last_name = 'doe', enter email='jdoe@email.com
+    #select radio button Gender : male
+    #mobile_number = 6564563989
+    # enter date_of_birth = 27 Nov 2022
+    # enter  subjects = 'selenium forms testing
+    # select checkboxes, select Sports, Reading
+    # upload picture
+    # enter message in text_area = '2906 Shell Rd, Brooklyn, NY, 12224"
+    # check if city list is enabled
+    # select state= NCR
+    # select city = Delhi
+    # check if Male gender is selected
+    # check if Sports is selected in Hobbies.
+    # click Submit
+    # verify the message = 'Thanks for submitting the form"
 
 
 except (Exception) as err:
