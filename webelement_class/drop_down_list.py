e188de4e82f1fd66ffac049d33133815d4719a33
iroda9900 <ishukurova0311@gmail.com>
2023-01-30 13:11:11 -0500

    test case updated and passed

--- webelement_class/drop_down_list.py
+++ webelement_class/drop_down_list.py
@@ -5,9 +5,11 @@ from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import *
 import time
 
+from selenium.webdriver.support.select import Select
+
 #from utilities import *
 
-HOST = "https://demoqa.com/automation-practice-form"
+HOST = "https://www.globalsqa.com/demo-site/select-dropdown-menu/"
 
 # created the object for chromedriver that talks to Chrome Browser
 chr_options = Options()
@@ -28,9 +30,33 @@ try:
     # All Locators (all values are ID locators):
     country_dd_tag = 'select'
 
+  # Steps:
+    driver.get(HOST)
+    time.sleep(5)
+
+    drop_down_elem = driver.find_element(By.TAG_NAME, country_dd_tag)
+    country_selection = Select(drop_down_elem)
+
+    print("check first selected option")
+    print('First selected option: ', country_selection.first_selected_option.text)
+    # select US from dropdown
+    print("Selecting by index 2")
+    country_selection.select_by_index(2)
+    print("Selected country:", country_selection.all_selected_options[0].text)
+    print("Selecting by value 'FRA'")
+    country_selection.select_by_value('FRA')
+    print("Selected country:", country_selection.all_selected_options[0].text)
+    print("Selecting by visible text 'United States")
+    country_selection.select_by_visible_text('United States')
+    print("# verify US is selected: get_all_selected option")
+    print("Selected country:", country_selection.all_selected_options[0].text)
 
 
 
+
+
+    time.sleep(2)
+    print("Dropdown Test completed successfully")
 
 except Exception as err:
     time.sleep(10)
